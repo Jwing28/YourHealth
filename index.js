8a3dd01fc60ee852304327757f2796d72db11be6
Jwing28 <jonathan.wing.lee@gmail.com>
2016-11-05 13:10:33 -0700

    [update] updated styles removed old code

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
-  
+  var activityLvl = "";
+
   $('.activityLvl').on('click','tr',function(event){
     event.preventDefault();
     $('.activityLvl tr').removeClass('bg-success');
-    $(this).addClass('bg-success');
+    $(this).addClass('bg-success');   
+    activityLvl = $(this)[0].innerText;
   });
 
-  $('.activityLvl').on('click','a',function(event){
-    event.preventDefault();
-    $('.activityLvl a').parent().eq(0).removeClass('bg-success');
-    $(this).parent().eq(0).addClass('bg-success');
-  });  
+  $('form').on('submit',function(event){
+    event.preventDefault(); //prevent pg refresh
+    var inputs = $('form :input');//arr
+    var keys = ['name','sex','age','height','weight'];
+    var userInput = {}; //user's info in form
 
+    userInput.activityMultiplier = parseFloat(activityLvl.slice(0,3));
 
+    inputs.each(function(index,value){
+      userInput[keys[index]] = value.value;
+    });
+
+    $('.summary').append('See Results');
+    $('.summary').addClass('bg-success');
+    $('.summary').css('font-size','20px');
+  });
 });
