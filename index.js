535cb265d5747b801ddd44dd9d2864efc4e14eb1
Jwing28 <jonathan.wing.lee@gmail.com>
2016-11-05 14:50:58 -0700

    [add] add localStorage for persistant data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+  localStorage.userInput = {}; //user's info: pass to summary
+  localStorage.setItem('myCat','Tom')
 $(document).ready(function() {
-  var activityLvl = "";
-
+  var activityLvl = ""; //activity level on selection
   $('.activityLvl').on('click','tr',function(event){
     event.preventDefault();
     $('.activityLvl tr').removeClass('bg-success');
@@ -12,16 +13,22 @@ $(document).ready(function() {
     event.preventDefault(); //prevent pg refresh
     var inputs = $('form :input');//arr
     var keys = ['name','sex','age','height','weight'];
-    var userInput = {}; //user's info in form
 
-    userInput.activityMultiplier = parseFloat(activityLvl.slice(0,3));
+    localStorage.userInput.activityMultiplier = parseFloat(activityLvl.slice(0,3));
 
     inputs.each(function(index,value){
-      userInput[keys[index]] = value.value;
+      localStorage.userInput[keys[index]] = value.value;
     });
 
-    $('.summary').append('See Results');
-    $('.summary').addClass('bg-success');
-    $('.summary').css('font-size','20px');
+    $('#summary').append('See Results');
+    $('#summary').addClass('bg-success');
+    $('#summary').css('font-size','20px');
+
+    $('.test').append(JSON.stringify(localStorage.userInput));
+    // document.cookie.input = {};
+    // document.cookie.input = userInput;
+
+
   });
 });
+
