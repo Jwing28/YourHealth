399f25662cd36d6705b59116622ada960b397d6d
Jwing28 <jonathan.wing.lee@gmail.com>
2016-11-04 23:45:14 -0700

    [update] updated activitylevel to table

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-  //works
-  console.log( document.getElementById('test').innerHTML );  
+  
+  $('.activityLvl').on('click','tr',function(event){
+    event.preventDefault();
+    $('.activityLvl tr').removeClass('bg-success');
+    $(this).addClass('bg-success');
+  });
 
   $('.activityLvl').on('click','a',function(event){
     event.preventDefault();
-    $('.activityLvl a').removeClass('bg-success');
-    $(this).addClass('bg-success');
-  });
-})
+    $('.activityLvl a').parent().eq(0).removeClass('bg-success');
+    $(this).parent().eq(0).addClass('bg-success');
+  });  
+
+
+});
