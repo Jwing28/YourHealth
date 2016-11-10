6c6134ad4c34a52d0b4880c0889668cea49ce014
Jwing28 <jonathan.wing.lee@gmail.com>
2016-11-09 16:47:39 -0800

    [add] add d3 bmr data

--- summary.js
+++ summary.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var results = {};
   var bmr = 0;
+  var bmi = 0;
   results.name = localStorage.getItem('name');
   results.sex = localStorage.getItem('sex');
   results.age = localStorage.getItem('age'); 
@@ -14,7 +15,9 @@ $(document).ready(function() {
   $('#height').append(" " + results.height);
   $('#weight').append(" " + results.weight);
 
-  $('#bmi').append(Math.round((results.weight / (Math.pow(results.height,2)) ) * 703));
+  bmi = Math.round((results.weight / (Math.pow(results.height,2)) ) * 703);
+
+  $('#bmi').append(bmi);
 
   if(results.sex === 'male') {
     bmr = bmrMen(results.height, results.weight, results.age)
@@ -40,14 +43,105 @@ $(document).ready(function() {
     return (bmr * multiplier).toFixed(2);
   }
 
+  var test = [{limit:25},bmi];
+  var bmr = [{limit:50}, {limit:32}, {user:40}]
+
+  // BMI attempt - 1. get two shapes going and one overlay the other
+  // 2 - get data from user into d3
+
   // d3
-  d3.select('.d3')
-    .append('svg')
-    .attr('height',50)
-    .attr('weight',50)
-    .append('circle')
-    .attr('cx',25)
-    .attr('cy',25)
-    .attr('r',5)
-    .style('fill','purple')
+
+  //.data joins test array data to current (not yet exist)
+  //p selection
+  var svgContainer = d3.select('.d3').append('svg')
+                                     .attr('width',100)
+                                     .attr('height',100);
+
+  var rectangles = svgContainer.selectAll('rect')
+                               .data(test)
+                               .enter()
+                               .append('rect');
+
+  var rectAttributes = rectangles
+                          .attr('x',function (d) {
+                            return typeof d === 'object' ? 25-9:d;
+                          })
+                          .attr('y',function (d) {
+                            return typeof d === 'object' ? 25:d;
+                          })
+                          .attr('height', function(d) {
+                            return typeof d === 'object' ? 25:d;
+                          })
+                          .attr('width', function(d) {
+                            return typeof d === 'object' ? 25 * 10:d * 10;
+                          })
+                          .style('fill',function(d) {
+                            return d < 25 ? 'lightblue':'gray';
+                          });                                   
+
+  var svgContainerBMR = d3.select('.bmr').append('svg')
+                                     .attr('width',200)
+                                     .attr('height',200);
+
+  var rectanglesBMR = svgContainerBMR.selectAll('rect')
+                               .data(bmr)
+                               .enter()
+                               .append('rect');                                                                    
+
+  var rectAttributesBMR = rectanglesBMR
+                          .attr('x',function (d) {
+                            if(d.limit || d.limit){
+                              return d.limit;
+                            }else {
+                              return d.user;
+                            }
+                          })
+                          .attr('y',function (d) {
+                            if(d.limit || d.limit){
+                              return d.limit;
+                            }else {
+                              return d.user;
+                            }
+                          })
+                          .attr('height', function(d) {
+                            if(d.limit || d.limit){
+                              return d.limit / 5;
+                            }else {
+                              return d.user / 5;
+                            }
+                          })
+                          .attr('width', function(d) {
+                            if(d.limit || d.limit){
+                              return d.limit * 10;
+                            }else {
+                              return d.user * 10;
+                            }
+                          })
+                          .style('fill',function(d) {
+                            if(d.limit > 35){
+                              return 'darkred';
+                            }else if(d.limit < 35 && d.limit > 30){
+                              return 'gray';
+                            }
+                            return 'lightblue'
+                          })                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
